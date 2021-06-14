0dd6a0c02ae3180cd5b3acbbc05d89c552b83796
Donald King <chronos@chronos-tachyon.net>
2021-06-13 18:45:28 -0700

    Reduce stutter.

--- assert.go
+++ assert.go
@@ -6,42 +6,42 @@ import (
 	"strings"
 )
 
-// Assert panics with AssertionError if cond is false.
+// Assert panics with Error if cond is false.
 func Assert(cond bool, text string) {
 	if cond {
 		return
 	}
-	panic(AssertionError{Text: text})
+	panic(Error{Text: text})
 }
 
-// Assertf panics with AssertionError if cond is false.
+// Assertf panics with Error if cond is false.
 func Assertf(cond bool, format string, v ...interface{}) {
 	if cond {
 		return
 	}
 	text := fmt.Sprintf(format, v...)
-	panic(AssertionError{Text: text})
+	panic(Error{Text: text})
 }
 
-// AssertNotNil takes a pointer to a nil-able type (pointer, interface, etc)
-// and panics with AssertionError if the pointed-to value is nil.
-func AssertNotNil(v interface{}) {
+// NotNil takes a pointer to a nil-able type (pointer, interface, etc) and
+// panics with Error if the pointed-to value is nil.
+func NotNil(v interface{}) {
 	r0 := reflect.ValueOf(v)
 	r1 := r0.Elem()
 	if !r1.IsNil() {
 		return
 	}
 	text := fmt.Sprintf("%s is nil", r1.Type().String())
-	panic(AssertionError{Text: text})
+	panic(Error{Text: text})
 }
 
-// AssertionError is the error type for Assert failure panics.
-type AssertionError struct {
+// Error is the error type for Assert failure panics.
+type Error struct {
 	Text string
 }
 
 // Error fulfills the error interface.
-func (err AssertionError) Error() string {
+func (err Error) Error() string {
 	var buf strings.Builder
 	buf.Grow(16 + len(err.Text))
 	buf.WriteString("AssertionError")
@@ -52,4 +52,12 @@ func (err AssertionError) Error() string {
 	return buf.String()
 }
 
-var _ error = AssertionError{}
+var _ error = Error{}
+
+// AssertNotNil is a compatibility alias for NotNil.
+func AssertNotNil(v interface{}) {
+	NotNil(v)
+}
+
+// AssertionError is a compatibility alias for Error.
+type AssertionError = Error
